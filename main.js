e26a66105d3b6f329e8a749a400a5781ece3fe09
kent.nguyen1 <kent.nguyen@laidon.com>
2023-04-09 23:02:47 +0700

    new files

---
+++ main.js
@@ -0,0 +1,13 @@
+var fullName = 'Nguyen Thanh Ket',
+    age = 31;
+
+// alert(fullName);
+// alert(age);
+
+setTimeout(function() {
+    prompt(fullName);
+}, 500);
+
+// setInterval(function() {
+//     prompt(age);
+// }, 2000);
